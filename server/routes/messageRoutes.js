2eba7d4a6e95c6365f7ed7b9ec653f957f87a2b8
Akshat <akshatsonibhl99@gmail.com>
2024-07-23 20:21:06 +0530

    chatPhase1

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { sendMessage } from "../controllers/messageController.js";
+import { sendMessage, getMessages } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 /* Post */
 router.post("/send/:id", verifyToken, sendMessage);
+
+/* GET */
+router.get("/:id", verifyToken, getMessages);
 
 export default router;
